937f0deb90729bd6fb4f55d67cb01c5ecd628f0f
Jonathan Henrique Maia de Moraes <arkye@hotmail.com.br>
2015-01-25 12:39:58 -0200

    Player Updates

--- behavior/Player Behavior.js
+++ behavior/Player Behavior.js
@@ -1,4 +1,4 @@
-#pragma strict
+﻿#pragma strict
 
 /*	PLAYER BEHAVIOR
  *	This script set standard actions to Player Character
@@ -10,9 +10,10 @@
  */
  
 // Constants
-var STANDARD_SPEED_X = 300;
-var STANDARD_SPEED_Y = 10;
-
+var STANDARD_SPEED_X = 180;
+var STANDARD_SPEED_Y = 5;
+//var PLAYER : GameObject;
+var ANIMATOR: Animator;
 // Self Attributes
 var jump: boolean;
 
@@ -29,23 +30,60 @@ function Start () {
 }
 
 function Update () {
+
+	
+	ANIMATOR = GetComponent("Animator");
 	if(isGameOver == false) {
-		if ((Input.GetAxis("Horizontal") == 0) && jump == false) {
+		if (Input.GetAxis("Horizontal") == 0) {
 			rigidbody2D.velocity = Vector2(0,rigidbody2D.velocity.y);
+			if( jump == false){
+				ANIMATOR.SetBool("Iddle",true);
+				ANIMATOR.SetBool("Walking",false);
+				ANIMATOR.SetBool("Jumping_Up",false);
+				ANIMATOR.SetBool("Jumping_Down", false);
+				ANIMATOR.speed = 0.35;
+				}
 		}
 		else if (Input.GetAxis("Horizontal") > 0) {
 			rigidbody2D.velocity = Vector2(STANDARD_SPEED_X * Time.deltaTime,rigidbody2D.velocity.y);
+			transform.localScale.x = 1.5;
+			if( jump == false){
+			ANIMATOR.SetBool("Iddle",false);
+			ANIMATOR.SetBool("Walking",true);
+			ANIMATOR.SetBool("Jumping_Up",false);
+			ANIMATOR.SetBool("Jumping_Down", false);
+			ANIMATOR.speed = 1.25;	
+			}
 		}
+		
 		else if (Input.GetAxis("Horizontal") < 0) {
 			rigidbody2D.velocity = Vector2(-STANDARD_SPEED_X * Time.deltaTime,rigidbody2D.velocity.y);
+			transform.localScale.x = -1.5;
+			if( jump == false){
+			ANIMATOR.SetBool("Iddle",false);
+			ANIMATOR.SetBool("Walking",true);
+			ANIMATOR.SetBool("Jumping_Up",false);
+			ANIMATOR.SetBool("Jumping_Down", false);
+			ANIMATOR.speed = 1.25;	
+			}
 		}
 
 		if ((jump == false) && (rigidbody2D.velocity.y == 0)){
-			if (Input.GetAxis("Vertical") > 0) {
+			if ((Input.GetAxis("Vertical") > 0) || Input.GetKey(KeyCode.Space)) {
 				rigidbody2D.velocity = Vector2(rigidbody2D.velocity.x, STANDARD_SPEED_Y);
 				jump = true;
+				ANIMATOR.SetBool("Jumping_Up", true);
+				ANIMATOR.SetBool("Iddle", false);
+				ANIMATOR.SetBool("Walking",false);
+				ANIMATOR.SetBool("Jumping_Down", false);
 			}
 		}
+		else if(rigidbody2D.velocity.y < 0){
+			ANIMATOR.SetBool("Jumping_Up", false);
+			ANIMATOR.SetBool("Jumping_Down", true);
+			ANIMATOR.SetBool("Iddle", false);
+			ANIMATOR.SetBool("Walking",false);
+			}
 	}
 }
 
@@ -53,6 +91,7 @@ function OnCollisionEnter2D(collide:Collision2D) {
 	if(isGameOver == false) {
 		var objectCollided = collide.gameObject;
 		if(isOnGround(objectCollided) == true) {
+		ANIMATOR.SetBool("Jumping_Down", false);
 			jump = false;
 		}
 		else if(isAnEnemy(objectCollided) == true) {
@@ -69,6 +108,7 @@ function isOnGround(objectCollided:GameObject) {
 		&& (objectCollided.tag == "Ground")
 		&& (objectCollided.transform.position.y < transform.position.y)) {
 		isOnGround = true;
+		ANIMATOR.SetBool("Jumping_Down", false);
 	}
 	return isOnGround;
 }
